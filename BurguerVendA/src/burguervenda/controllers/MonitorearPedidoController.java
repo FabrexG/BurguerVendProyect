981de0df9ea849e9513f6f38304a90ea358ed3d4
FabrexG <brian.escalona@alumnos.uacm.edu.mx>
2024-11-17 21:33:59 -0600

    Funcionalidad del monitoreo de pedido
    Se agrego la funcionalidad del monitoreo del pedido, ahora el sistema se comporta como debería de hacerlo

--- BurguerVendA/src/burguervenda/controllers/MonitorearPedidoController.java
+++ BurguerVendA/src/burguervenda/controllers/MonitorearPedidoController.java
@@ -6,7 +6,11 @@ package burguervenda.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -32,21 +36,84 @@ public class MonitorearPedidoController implements Initializable {
 
     @FXML
     private Button btnMensaje;
-
+    
+     @FXML
+    private Label lbCodigo;
+    
+    @FXML
+    private Label lbTiempo;
+     
     @FXML
     private Label lbRestaurante;
 
     @FXML
     private ProgressBar pbMonitoreo;
+    
+    private String codigoEntrega;
+    
+    private String codigoVerificacion;
+    
+    private int time = 120; 
+    
+    private static final String CARACTERES = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    private boolean running = true;
 
     @FXML
     void abrirChat(ActionEvent event) throws IOException {
+        running = false;
         Parent root = FXMLLoader.load(getClass().getResource("/burguervenda/vistas/BotChat.fxml"));
         anchorPane.getChildren().setAll(root);
     }
+    
+    public String generarCodigo(int len) {
+        Random rand = new Random();
+        StringBuilder codigo = new StringBuilder(len);
+        for(int i = 0; i < len; i++) {
+            int index = rand.nextInt(CARACTERES.length());
+            codigo.append(CARACTERES.charAt(index));
+        }
+        return codigo.toString();
+    }
+    @FXML
+    void confirmarEntregaPedido() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/burguervenda/vistas/ConfirmarEntregaPedido.fxml"));
+        Parent root = loader.load();
+        ConfirmarEntregaPedidoController controller = loader.getController();
+        controller.setCodigo(codigoVerificacion);
+        anchorPane.getChildren().setAll(root);
+    }
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i = 1; i <= 100 && running; i++) {
+                    updateProgress(i, 100);
+                    Thread.sleep(50);
+                }
+                return null;
+            }
+        };
+        codigoEntrega = generarCodigo(4);
+        codigoVerificacion = generarCodigo(4);
+        lbCodigo.setText(codigoEntrega);
+        System.out.println(codigoVerificacion);
+        
+        task.setOnSucceeded(event -> {
+            if (pbMonitoreo.getProgress() >= 1.0) {
+                try {
+                    confirmarEntregaPedido();
+            } catch (IOException ex) {
+                    Logger.getLogger(MonitorearPedidoController.class.getName()).log(Level.SEVERE, null, ex);
+             }
+            }
+        });
+        pbMonitoreo.progressProperty().bind(task.progressProperty());
+        
+        new Thread(task).start(); 
     }    
     
 }
